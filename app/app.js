712098892809a3688870ec3d451a459f73cc81f2
Seongmin <lll2slll@naver.com>
2019-09-26 15:03:49 +0900

    [2019. 09. 26] 1. Upload 저장소 변경 > Local -> S3

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@ import Koa from 'koa';
 import errorHandler from 'middlewares/errorHandler';
 import responseHandler from 'middlewares/responseHandler';
 import requestId from 'koa-requestid';
+import serve from 'koa-static-server';
 import bodyParser from 'koa-bodyparser';
 import cors from 'koa2-cors';
 import helmet from 'koa-helmet';
@@ -16,6 +17,7 @@ const app = new Koa();
 app.use(responseHandler());
 app.use(errorHandler());
 app.use(requestId());
+app.use(serve({ rootDir: 'upload', rootPath: '/upload' }));
 app.use(bodyParser({
     enableTypes: ['json', 'form'],
     jsonLimit: '10mb',
